17aa3bc656804103068ef63bf25f64bbf2e07abf
René Meier <renemeier.de@gmail.com>
2025-02-03 22:25:55 -0300

    whatsapp cta link fix

--- src/components/ui/WhatsAppIcon.jsx
+++ src/components/ui/WhatsAppIcon.jsx
@@ -1,9 +1,13 @@
 import './WhatsAppIcon.css';
 
 const WhatsAppIcon = () => {
+  const phoneNumber = '5519984308466';
+  const urlMessage =
+    'Hi%20Ren%C3%A9%21%0AI%20found%20you%20in%20your%20portfolio%21';
+
   return (
     <a
-      href="Hi%20Ren%C3%A9%21%0AI%20found%20you%20in%20your%20portfolio%21"
+      href={`https://api.whatsapp.com/send/?phone=${phoneNumber}&text=${urlMessage}&type=phone_number&app_absent=0`}
       rel="noopener noreferrer"
       className="float"
       target="_blank"
