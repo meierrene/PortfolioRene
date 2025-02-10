1520544420869de1cb2ae02115b5b355e817b163
unknown <renemeier.de@gmail.com>
2025-02-10 14:29:29 -0300

    styling tweak

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,15 @@
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p className="footer-copyright">
-        Copyright &copy; {new Date().getFullYear()} by René Meier
+      <p className="description">
+        Copyright &copy; {new Date().getFullYear()} by René Meier.
+        <br />
+        This App was built with React.JS
+        <span className="react-icon">
+          <img src="react.svg" alt="React Logo spinning" />
+        </span>
       </p>
     </div>
   );
